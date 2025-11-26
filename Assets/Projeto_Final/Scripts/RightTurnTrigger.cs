f932d7eb42597bc9453760b0c95e1b92963578e4
Francisco Vilaça <francisco07032003@gmail.com>
2025-11-26 15:00:12 +0000

    Player and camera rotation on curves

--- Assets/Projeto_Final/Scripts/RightTurnTrigger.cs
+++ Assets/Projeto_Final/Scripts/RightTurnTrigger.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class RightTurnTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     private MovePlatform parentMover;
 
     void Start()
@@ -16,8 +17,11 @@ public class RightTurnTrigger : MonoBehaviour
         {
             Debug.Log("RightTurnTrigger activated");
             //-10f Porque da a seta azul ao contrario?
-            Vector3 newDirection = other.transform.forward * -10f;
-            parentMover.SetMoveDirection(newDirection);
+            Vector3 newDirection = other.transform.forward * 10f;
+            parentMover.SetMoveDirection(-newDirection);
+            
+            Quaternion newRotation = Quaternion.LookRotation(newDirection, Vector3.up);
+            player.transform.rotation = newRotation;
         }
     }
 
